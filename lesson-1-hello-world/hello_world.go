1c62552d272ae97b478eb410de2d9ce668971538
Sai Shashaank V V <saishashaankvv@gmail.com>
2024-06-04 12:01:14 +0100

    refactored code

--- lesson-1-hello-world/hello_world.go
+++ lesson-1-hello-world/hello_world.go
@@ -2,9 +2,36 @@ package main
 
 import "fmt"
 
-func Hello(greeting string) string {
-	return "Hello " + greeting
+const (
+	Spanish = "Spanish"
+	French  = "French"
+
+	FrenchHelloPrefix  = "Bonjour, "
+	SpanishHelloPrefix = "Hola, "
+	EnglishHelloPrefix = "Hello, "
+)
+
+//One way to write a function
+func Hello(name string, language string) string {
+	if name == "" {
+		name = "World"
+	}
+	return resolveGreetingPrefix(language) + name
 }
+
+//Another way to write a function
+func resolveGreetingPrefix(language string) (prefix string) {
+	switch language {
+	case French:
+		prefix = FrenchHelloPrefix
+	case Spanish:
+		prefix = SpanishHelloPrefix
+	default:
+		prefix = EnglishHelloPrefix
+	}
+	return
+}
+
 func main() {
-	fmt.Println(Hello("my twin"))
+	fmt.Println(Hello("my twin", ""))
 }
--- lesson-1-hello-world/hello_world_test.go
+++ lesson-1-hello-world/hello_world_test.go
@@ -3,11 +3,28 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Chris", "")
+		want := "Hello, Chris"
+		assertTestMessage(t, got, want)
+	})
 
-	got := Hello("my twin")
-	want := "Hello my twin"
+	t.Run("saying 'Hello, World' when an empty string is applied", func(t *testing.T) {
+		got := Hello("", "")
+		want := "Hello, World"
+		assertTestMessage(t, got, want)
+	})
 
+	t.Run("saying 'Hello' in language chosen by person", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
+		assertTestMessage(t, got, want)
+	})
+}
+
+func assertTestMessage(t testing.TB, got, want string) {
+	t.Helper()
 	if got != want {
-		t.Errorf("Tests failed, Got %q but wanted %q", got, want)
+		t.Errorf("Test failed - got %q, wanted %q", got, want)
 	}
 }
